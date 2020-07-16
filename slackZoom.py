3102ad725722547d0ff8055cfcf8f6000358a61c
Yukimitsu Izawa <you@example.com>
2020-07-16 11:15:59 +0900

    restore the previous status of Slack.

--- slackZoom.py
+++ slackZoom.py
@@ -14,18 +14,21 @@ class Slack:
     def __init__(self):
         self.token = os.environ["SLACK_TOKEN"]
         self.user_id = os.environ["SLACK_USER_ID"]
+        self.headers = {"Authorization": "Bearer %s" % self.token, "X-Slack-User": self.user_id, "Content-Type": "application/json; charset=utf-8"}
 
     def set_status(self, status_emoji, status_text):
-        headers = {"Authorization": "Bearer %s" % self.token, "X-Slack-User": self.user_id, "Content-Type": "application/json; charset=utf-8"}
         payload = {"profile": {"status_emoji": status_emoji, "status_text": status_text}}
-        res = requests.post("https://slack.com/api/users.profile.set", data=json.dumps(payload), headers=headers).json()
+        res = requests.post("https://slack.com/api/users.profile.set", data=json.dumps(payload), headers=self.headers).json()
 
-    def get_status():
-        pass
-
+    def get_status(self):
+        payload = {}
+        res = requests.post("https://slack.com/api/users.profile.get", data=json.dumps(payload), headers=self.headers).json()
+        return({'status_emoji' : res['profile']['status_emoji'],
+                'status_text' : res['profile']['status_text']})
+        
 class StateMachine:
     def __init__(self):
-        self.stat = "unknown"
+        self.stat = False
 
     def get_state(self):
         return(self.stat)
@@ -45,9 +48,10 @@ while True:
     status = Zoom.checkZoom()
     if(myMachine.set_state(status)):
         if(status):
+            prev_status = mySlack.get_status()
             mySlack.set_status(":zoom:", "In a meeting via Zoom")
         else:
-            mySlack.set_status("", "")
+            mySlack.set_status(prev_status["status_emoji"], prev_status["status_text"])
     else:
         pass
     time.sleep(3)
